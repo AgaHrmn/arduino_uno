77555bbabec4d19d6f49de60c578ec950c6078c9
agaolejniq <a.olejnikk@gmail.com>
2025-07-08 22:05:58 +0200

    adc

--- adc/adc_4_leds/adc_4_leds.ino
+++ adc/adc_4_leds/adc_4_leds.ino
@@ -7,7 +7,6 @@ int ADCvalue = 0;
 int ADCscaled = 0;
 
 void setup() {
-  Serial.begin(9600);
 
   pinMode(red,OUTPUT);
   pinMode(yellow,OUTPUT);
@@ -20,35 +19,30 @@ void loop() {
 
   switch(ADCscaled) {
     case 1:
-      Serial.println("red");
       digitalWrite(red, HIGH);
       digitalWrite(yellow, LOW);
       digitalWrite(green, LOW);
       digitalWrite(blue, LOW);
       break;
     case 2:
-      Serial.println("yellow");
       digitalWrite(red, LOW);
       digitalWrite(yellow, HIGH);
       digitalWrite(green, LOW);
       digitalWrite(blue, LOW);
       break;
     case 3:
-      Serial.println("green");
       digitalWrite(red, LOW);
       digitalWrite(yellow, LOW);
       digitalWrite(green, HIGH);
       digitalWrite(blue, LOW);
       break;
     case 4:
-      Serial.println("blue");
       digitalWrite(red, LOW);
       digitalWrite(yellow, LOW);
       digitalWrite(green, LOW);
       digitalWrite(blue, HIGH);
       break;
     case 5:
-      Serial.println("all");
       digitalWrite(red, HIGH);
       digitalWrite(yellow, HIGH);
       digitalWrite(green, HIGH);
